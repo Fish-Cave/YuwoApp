4a60eee46f19b91573d779bc72dbb29d16f7e8c0
1inkun <zxc1830597113@gmail.com>
2025-02-24 11:57:55 +0800

    主界面1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,11 @@
 <template>
 	<view>
+		<!--导航栏？-->
 		<view class="Header">
-			<navigator>← 前一天</navigator>
+			<view class="arrow">
+				<img class="icon" src="/static/icon/arrow_left.png" />
+				<navigator>前一天</navigator>
+			</view>
 			<view class="item">
 				<view>
 					<text>当前位置:&nbsp</text>
@@ -11,23 +15,38 @@
 					<text >{{nowDate}}</text><br/>
 				</view>
 				<view>
-					<text>日历</text>
+					<img class="icon" src="/static/icon/calendar.png" />
 				</view>					
 			</view>
-			<navigator>后一天 →</navigator>
+			<view class="arrow">
+				<navigator>后一天</navigator>
+				<img class="icon" src="/static/icon/arrow_right.png" />
+			</view>
 		</view>
-		<hr />
+		<hr/>
 		<text style="font-size: 10rpx; color: gray;">&nbsp&nbsp&nbsp&nbsp点击机台可查看机台信息</text>
+		
+		<!--机台详细信息，使用了uniapp uni-ui的卡片分组-->
+		<!--但是uni-ui的组件在微信开发工具的小程序模拟器中渲染有问题，之后可能要修改 -->
 		<uni-section title="IIDX" type="line">
 			<uni-group mode="card">
-				<view>
-					<view> 分组内容 </view>
-					<view> 分组内容 </view>
-					<view> 分组内容 </view>
+				<view style="display: flex;">
+					<view class="item">
+						<text>当前预约玩家</text>
+						<text>玩家</text>
+						<text>玩家</text>
+						<text>玩家</text>
+						<text>玩家</text>
+						<text>玩家</text>
+					</view>>
+					<view style="display: flex; flex-direction: column;">
+						<text>在此显示机台详细信息</text>
+					</view>
 				</view>
-				
+				<button class="orderButton" @click = "test()">预约此机台</button>
 			</uni-group>
 		</uni-section>
+		
 		<uni-section title="SDVX" type="line">
 			<uni-group mode="card">
 				<view> 分组内容 </view>
@@ -36,6 +55,7 @@
 				<view> 分组内容 </view>
 			</uni-group>
 		</uni-section>
+		
 		<uni-section title="舞萌DX" type="line">
 			<uni-group mode="card">
 				<view> 分组内容 </view>
@@ -44,6 +64,7 @@
 				<view> 分组内容 </view>
 			</uni-group>
 		</uni-section>
+		
 		<uni-section title="中二节奏" type="line">
 			<uni-group mode="card">
 				<view> 分组内容 </view>
@@ -52,6 +73,7 @@
 				<view> 分组内容 </view>
 			</uni-group>
 		</uni-section>
+		
 		<uni-section title="音击" type="line">
 			<uni-group mode="card">
 				<view> 分组内容 </view>
@@ -60,6 +82,7 @@
 				<view> 分组内容 </view>
 			</uni-group>
 		</uni-section>
+		
 		<uni-section title="太鼓达人" type="line">
 			<uni-group mode="card">
 				<view> 分组内容 </view>
@@ -80,18 +103,18 @@
 	}
 </script>
 <style lang="scss">
+	/*疑似导航栏使用*/
+	
+	/*容器内组件水平排列*/
 	.Header {
 		display: flex;
-		/* 主轴（水平）居中 */
 		justify-content: center;
-		/* 交叉轴（垂直）居中 */
 		align-items: center;
-		/* 可选：设置容器高度（垂直居中需要高度支持） */
 		height: 160rpx;
-		/* 可选：子元素间距 */
 		gap: 50rpx;
 	  
 	}
+	/*容器内组件竖直排列*/
 	.item{
 		display: flex;
 		flex-direction: column;
@@ -110,5 +133,30 @@
 		justify-content: center;
 		align-items: center;
 	}
-
+	/*图标*/
+	.icon{
+		height: 35rpx;
+	}
+	.arrow{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 5rpx;
+	}
+	/*分割线*/
+	hr{
+		margin-left: 30rpx;
+		margin-right: 30rpx;
+	}
+	/*预约按钮*/
+	.orderButton{
+		margin-top: 35rpx;
+		width: 400rpx;
+		height: 90rpx;
+		opacity: 1;
+		border-radius: 40px;
+		background: rgba(255, 195, 0, 1);
+		box-shadow: 0px 2px 4px  rgba(0, 0, 0, 0.25);
+	}
+	/*我不会写样式*/
 </style>
