b486b61a5890cf1c5499f0f834d18067cbae7faa
1inkun <zxc1830597113@gmail.com>
2025-02-25 11:21:53 +0800

    1

--- pages/recharge/recharge.vue
+++ pages/recharge/recharge.vue
@@ -16,18 +16,14 @@
 				</view>
 			</view>
 		</uni-group>
+		
 		<view style="padding-left: 30rpx; padding-right: 30rpx;">
 			<view style="display: flex; justify-content: space-between;">
 				<view style="display: flex; flex-direction: column;">
-					<uni-title type="h1" title="剩余时长"></uni-title>
 					<uni-title type="h1" title="鱼窝会员"></uni-title>
 					<uni-title type="h1" title="大月卡/大周卡"></uni-title>
 				</view>
 				<view style="display: flex; flex-direction: column; justify-content: space-around;">
-					<view>
-						<text style="color: rgba(255, 141, 26, 1);">{{}}</text>
-						<text>小时</text>
-					</view>
 					<view>
 						<text style="color: rgba(255, 141, 26, 1);">{{}}</text>
 						<text>到期</text>
@@ -37,8 +33,29 @@
 					</view>
 				</view>
 			</view>
+			<view>
+				<text class="tips">
+					如果已经成为会员，计时计费将会使用会员价格。
+				</text>	
+			</view>
 			
+			<!--BLANK-->
+			<view style="height: 500rpx;"></view>
 			
+			<!--会员充值模块-->
+			<view>
+				<view>
+					
+				</view>
+				<view style="display: flex;">
+					<view style="display: flex; justify-content: center;
+					 align-content: center; align-self: center;">
+						<uni-icons type="info" size="50rpx"></uni-icons>
+						<navigator class="tips">关于鱼窝会员、大月卡、大周卡的区别</navigator>
+					</view>
+					<button class="bt">购买</button>
+				</view>
+			</view>
 		</view>
 	</view>
 		
@@ -70,8 +87,8 @@
 		align-items: center;
 	}
 	.bt{
-		width: 200rpx;
-		height:40px;
+		width: 160rpx;
+		height:80rpx;
 		border-radius: 40px;
 		background: rgba(255, 195, 0, 1);
 		box-shadow: 0px 2px 4px  rgba(0, 0, 0, 0.25);
