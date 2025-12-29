6c922bab0aea29387a6581813fb4fbf98880ac62
1inkun <zxc1830597113@gmail.com>
2025-12-29 20:08:53 +0800

    2026 holiday update

--- modules/isFreeDay.ts
+++ modules/isFreeDay.ts
@@ -1,25 +1,41 @@
 import dayjs from 'dayjs';
-import holiday2025 from '@/static/holiday/2025.json'
+import holiday2025 from '@/static/holiday/2025.json';
 
-export default function isFreeDay(timestamp?:number){
-	const now = dayjs(timestamp).format('YYYY-MM-DD')
-	console.log("isFreeDay" + now)
-	const next = dayjs(timestamp).add(1,'day').format('YYYY-MM-DD')
-	console.log("NextDay" + next)
-	const result = holiday2025.days.find(data =>data.date == next)
-	if(result!=null){
-		if(result.isOffDay){
-			return false
+export default function isFreeDay(timestamp?: number) {
+	const now = dayjs(timestamp).format('YYYY-MM-DD');
+	// console.log("isFreeDay " + now)
+	const next = dayjs(timestamp).add(1, 'day').format('YYYY-MM-DD');
+	// console.log("NextDay " + next)
+	const nextDayResult = holiday2025.days.find((data) => data.date == next);
+	const nowDayResult = holiday2025.days.find((data) => data.date == now) || 'empty';
+	console.log(nowDayResult);
+	console.log(nextDayResult);
+	if (nextDayResult != undefined) {
+		if (nextDayResult.isOffDay) {
+			return !nextDayResult.isOffDay;
+		} else {
+			if (nowDayResult != 'empty') {
+				return !nowDayResult.isOffDay;
+			}else{
+				return !nextDayResult.isOffDay;
+			}
 		}
-		return true
-	}else{
-		if(holiday2025.days.find(data =>data.date == now)?.isOffDay){
-			return false
+	} else {
+		const res = holiday2025.days.find((data) => data.date == now);
+		console.log(res);
+		if (res != undefined) {
+			if (res?.isOffDay) {
+				return !res.isOffDay;
+			} else {
+				return true;
+			}
 		}
-		const today = dayjs(timestamp).day()
-		if(0<today&&today<5){
-			return true
+		const today = dayjs(timestamp).day();
+		console.log(today);
+		if (0 < today && today < 5) {
+			return true;
 		}
-		return false
+		return false;
 	}
+
 }
